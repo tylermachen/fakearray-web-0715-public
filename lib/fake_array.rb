cbc62d56e257c0f01edbae1758a1222e470fe03b
Tyler Machen <tylermachen@gmail.com>
2015-08-05 20:37:24 -0400

    complete fake array

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,25 @@
+require 'pry'
+
 class FakeArray
+  attr_accessor :nums
 
-  # you'll need a splat in this class somewhere
+  def initialize(*nums)
+    @nums = nums
+  end
 
+  def [](num)
+    @nums[num]
+  end
 
+  def each
+    i = 0
+    while i < @nums.length
+      yield @nums[i]
+      i += 1
+    end
+  end
 
+  def first
+    @nums.first
+  end
 end
